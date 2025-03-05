e46d3b44a17e187acc7ded12d8ec2857e2281283
mikamttr <mika348@live.fr>
2025-03-05 09:20:28 +0100

    added CORS login and signup to frontend

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const cors = require('cors')
+
 const port = process.env.PORT || 5000
-const colors = require('colors')
+const frontendUrl = process.env.FRONTEND_URL
 
 const app = express()
+
+app.use(cors({
+    origin: frontendUrl,
+    credentials: true
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -11,5 +18,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api', require('./routes'))
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`API running on port ${port}`)
+    console.log(`CORS allowed for: ${frontendUrl}`)
 })
